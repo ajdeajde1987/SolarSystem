49963c8a1df7d4346859d3d906a2326925eff94e
anako <hope.love.faith87@gmail.com>
2021-11-06 11:53:58 -0400

    feature Sun added with a bug that needs to be resolved

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	SolarSystem andromeda=new SolarSystem();
-	andromeda.
+	andromeda.Sun=1;
+	andromeda.planet=2;
+	andromeda.moon=4;
+
+	featureSun mikeSun=new featureSun();
+	mikeSun.color="blue";
+	mikeSun.heat="6778676767";
+	mikeSun.temp="5celsius";
+	mikeSun.radius="0.009";
+	mikeSun.maritalStatus="single";
+
     }
 }
